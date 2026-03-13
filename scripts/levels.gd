6bdee82b9615467401cf6ab524d491a87a5522aa
Dmitriy <dimamoora@gmail.com>
2026-03-13 14:01:04 +0600

    Обновление: текстура шарика

--- scripts/levels.gd
+++ scripts/levels.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
 var levels = [
-	"res://levels/1.tscn",
+	"res://levels/7.tscn",
 	"res://levels/2.tscn",
 	"res://levels/3.tscn",
 	"res://levels/4.tscn",
 	"res://levels/5.tscn",
-	"res://levels/6.tscn"
+	"res://levels/6.tscn",
+	"res://levels/7.tscn"
 ]
 
 var current_level_index = 0
