176a021e630c94252b0aece70a8c6b5235840984
Dmitriy <dimamoora@gmail.com>
2026-04-05 18:20:04 +0600

    Исправлена ошибка изменения размера шарика

--- scripts/mini.gd
+++ scripts/mini.gd
@@ -4,10 +4,15 @@ extends Node2D
 @export var big_val: float = 1.0
 
 var is_shrinking: bool = true
+var inside = false
 
 func _on_entry_big_body_entered(body):
 	if body.has_method("change_ball_size"):
 		is_shrinking = true
+func _on_entry_big_body_exited(body: Node2D) -> void:
+	if not inside:
+		body.change_ball_size(big_val)
+		is_shrinking = false
 
 func _on_entry_small_body_entered(body):
 	if body.has_method("change_ball_size"):
@@ -15,5 +20,10 @@ func _on_entry_small_body_entered(body):
 
 func _on_center_zone_body_entered(body: Node2D) -> void:
 	if body.has_method("change_ball_size"):
+		inside = true
 		var target = small_val if is_shrinking else big_val
 		body.change_ball_size(target)
+
+func _on_center_zone_body_exited(body: Node2D) -> void:
+	if body.has_method("change_ball_size"):
+		inside = false
