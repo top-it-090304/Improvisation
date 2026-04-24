e81a3ecdb43bc410921fd2ee1a0842228eafdd13
Dmitriy <dimamoora@gmail.com>
2026-04-24 18:32:54 +0600

    Добавлен уровень 12

--- scripts/main.gd
+++ scripts/main.gd
@@ -17,7 +17,8 @@ var levels = [
 	"res://levels/8.tscn",
 	"res://levels/9.tscn",
 	"res://levels/10.tscn",
-	"res://levels/11.tscn"
+	"res://levels/11.tscn",
+	"res://levels/12.tscn"
 ]
 
 var main_menu_pos = Vector2(-64, -64)
